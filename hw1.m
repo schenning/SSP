71c315ad82e81dedfef30db9c82cdc42fc33e993
Henning Schei <schei@amiata.eurecom.fr>
2015-10-14 15:28:25 +0200

    asdf

--- hw1.m
+++ hw1.m
@@ -1,7 +1,18 @@
-% HW ,
+% HW , Henning Schei
+%clear all;
+%close all; 
+
+% fileID = fopen('hw_host_ip_list.txt','r');
+% fSpec = '%s';
+% cmp =fscanf(fileID, fSpec);
+% fclose(fileID);
+
+data = load('hw_host_ip_list.txt');
 
 
-x = fileread('hw_host_ip_list.txt');
+
+ 
+
 
 %[res] = pingstats(x(2), 100, 'v'); 
 
