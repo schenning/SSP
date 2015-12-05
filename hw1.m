7854f8beb7b3e9b58f06ca69339838d4edb6930a
Henning <henninsc@stud.ntnu.no>
2015-12-05 14:52:49 +0100

    Only shifted reydely left

--- hw1.m
+++ hw1.m
@@ -8,14 +8,11 @@
 choise = 0;
 
 if (choise)
-    fileID = fopen('hw_host_ip_list.txt','r');
-    fSpec  = '%s';
-    data   = textscan(fileID, fSpec);
-    fclose(fileID);
+  
     res=zeros(1000,4);
     res(1:1000,1) = pingstats('mercury.iet.ntnu.no' , 1000, ''); % Norwegian Univ. of Science and Technology #1 in res
     res(1:1000,2) = pingstats('atalante.stanford.edu', 1000,''); % Standford University                      #2 in res
-    res(1:1000,3) = pingstats('mx.vvsu.ru',1000,''); % Vladivostok State University ofEconimics and Service  #3 in res
+    res(1:1000,3) = pingstats('mx.vvsu.ru',1000,''); % Vladivostok State University of Econimics and Service  #3 in res
     res(1:1000,4) = pingstats('197.255.176.1',1000,''); %Brazzaville                                         #didn't work
 
     %res(1:100,3) = pingstats('archlinux.uib.no',100,'');
@@ -31,66 +28,136 @@ end
 
 
 
-<<<<<<< HEAD
-fileID = fopen('hw_host_ip_list.txt','r');
-fSpec  = '%s';
-data   = textscan(fileID, fSpec);
-fclose(fileID);
 
-congo = zeros(1,1000);
-congo(1:1000)=pingstats('197.214.128.2', 1000,'');
 
-%res=zeros(1000,4);
-%res(1:1000,1) = pingstats('mercury.iet.ntnu.no' , 1000, ''); % Norwegian Univ. of Science and Technology
-%res(1:1000,2) = pingstats('atalante.stanford.edu', 1000,''); % Standford University
-%res(1:1000,3) = pingstats('mx.vvsu.ru',1000,''); % Vladivostok State University ofEconimics and Service 
-%res(1:1000,4) = pingstats('197.255.176.1',1000,''); %Brazzaville
+data = res(1:1000,1);
+%% Problem iii) Calculating $$\Theta_{ML_{i}} \ \ $ for each of the distributions 
 
-%res(1:100,3) = pingstats('archlinux.uib.no',100,'');
-% 217.74.123.50 % ISP = Vladivostok SU
-% 197.214.128.4 Airtel Congo.
-% 197.255.176.1 Brazzaville
-% 197.220.64.1 , Mogadishu
-=======
->>>>>>> d7205de415656af0cdef4ddfbe09a59fb1fda961
-%%
+% Gaussian distribution
 
-% Plotting histograms
+mu_G = sum(data)/length(data);
+tmp=0;
+for i = 1:length(data)
+    tmp = tmp + power(data(i) - mu_G,2);
+end
+ro_G = tmp/length(data); % Divide by n+1?
+
+% Rayleigh
+ro_R = power(2*length(data),-1) * sum (data.^2);
+
+
+% Erlang distribution
+m = [1,2,3];
+lambda_E = (m*length(data))/sum(data);
+
+% Shifted exponential distribution
+alpha_exp = min(data);
+tmp=0;
+for i =1:length(data)
+    tmp = tmp + data(i) - alpha_exp; 
+end
+
+lambda_exp = length(data)/tmp;
+
+
+% Shifted Rayleigh
+% To be continued
+
+
+%% Problem iv: Superimpose graphs of histograms and marginal denseties
+
+% pdf's:
+
+f_G = (1./sqrt(2.*pi.*ro_G)) .* exp(-power( (1:1000)-mu_G,2)./(2.*ro_G));
+f_R = ((1:1000)./ro_R).*exp (-(power((1:1000),2))./(2.*ro_R ));
+M = 1;
+f_E1 = (power(lambda_E(1),M+1)./(factorial(M))).*power((1:1000),M).*exp(-lambda_E(1).*(1:1000));
+M = 2;
+f_E2 = (power(lambda_E(2),M+1)./(factorial(M))).*power((1:1000),M).*exp(-lambda_E(2).*(1:1000));
+M = 3;
+f_E3 = (power(lambda_E(3),M+1)./(factorial(M))).*power((1:1000),M).*exp(-lambda_E(3).*(1:1000));
+f_exp = zeros(1,1000);
+for i=1:1000
+    if i< alpha_exp
+        f_exp(i)=0;
+    elseif i>=alpha_exp
+        f_exp(i) = lambda_exp .* exp(-lambda_exp.*(i-alpha_exp));
+    end
+end
+
+alpha_SR = min(data);
+f_SR = zeros(1,1000);
+ro_SR=100;
+for i =1:1000
+    if i<alpha_SR
+        f_SR(i)=0;
+    elseif i>=alpha_SR
+        f_SR(i)= ((i-alpha_SR)./ro_SR)*exp(((i-alpha_SR).^2)./(2*ro_SR)); 
+    end
+end
+
+
+
+% Plotting histograms and margnial densisies
+
+subplot(4,2,1)
 hist(res(:,1), 500);
 xlabel 'msec'
 ylabel '#ping'
 title 'mercury.iet.ntnu.no - Located in Trondheim, Norway';
-figure;
-hist(res(:,2),500);
+subplot(4,2,2)
+plot(f_G)
+xlim([0 max(data)+100])
 xlabel 'msec'
-ylabel '#ping'
-title 'atlante.standford.edu - Located in California';
-figure;
-hist(res(:,3),500);
-xlabel 'msec'
-ylabel '#ping'
-title 'mx.vvsu.ru - Located in Vladivostok, Russia.';
+ylabel 'Pr'
+title  'Gaussian distribution'
+subplot(4,2,3)
+plot(f_E1);
+xlim([0 max(data)+100])
+xlabel 'm'
+title 'Erlang distribution m=1'
+subplot(4,2,4)
+plot(f_E2)
+xlim([0 max(data)+100])
+title 'Erlang distribution m=2'
+subplot(4,2,5)
+plot(f_E3)
+xlim([0 max(data)+100])
+title 'Erlang distribution m=3'
+subplot(4,2,6)
+plot(f_exp)
+xlim([0 max(data)+100])
+title 'Shifted exponential distribution'
+subplot(4,2,7)
+plot(f_R)
+xlim([0 max(data)+100])
+title 'Rayleigh distribution'
+subplot(4,2,8)
+plot(f_SR)
+xlim([0 max(data)+100])
+title 'Shifted Reyleigh distribution'
 
 
 
-%% Problem i) Maximum Likelihood estimation 
+% 
+% 
+% figure;
+% hist(res(:,2),500);
+% xlabel 'msec'
+% ylabel '#ping'
+% title 'atlante.standford.edu - Located in California';
+% figure;
+% hist(res(:,3),500);
+% xlabel 'msec'
+% ylabel '#ping'
+% title 'mx.vvsu.ru - Located in Vladivostok, Russia.';
+% close all
 
 
-dis = { 'Normal', 'Rayleigh','Exponential'}; % The Erlang dist was not a part of the possibilities
 
-phat = zeros(100,3);
-for i=1:3
-    phat(:,i) = mle(res(:,i),'distribution','Rayleigh');
-    
-end
 
-mu_n=0; ro_n=0; %mu_normaldist and ro_normaldist
 
-% Need separate for-loop bc of two parameters in normal dist
 
-for i=1:3
-    [mu_n, ro_n] = mle(res(:,i), 'distribution', 'Normal');
-end
 
 
 
