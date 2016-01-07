ca0c7c5467c297ed3383e1889410e843a1482de3
Henning <henninsc@stud.ntnu.no>
2016-01-07 23:54:35 +0100

    tp1

---
+++ tp1/levdur.m
@@ -0,0 +1,23 @@
+function [a,k,Jo]=levdur(r)
+
+	
+    % Input: r(m)
+	p=20;
+    a=zeros(p+1,1); % Prediction error filter
+    k=zeros(p,1);
+    Jo=zeros(p+1,1); 
+    % Initialization
+    J=r(1); Jo(1)=J;
+    beta=r(2); 
+	k(1)=-beta/J; 
+	a(1)=k(1);
+    J=J+beta*k(1); 
+	Jo(2)=J;
+    % Recursion
+    for m=2:p
+        beta=(r(2:m))'*flipud(a(1:m-1))+r(m+1);
+        k(m)=-beta/J;
+        a(1:m)=[(a(1:m-1))' 0]'+[(flipud(a(1:m-1)))' 1]'*k(m);
+        J=J+beta*k(m); Jo(m+1)=J;
+    end
+    a(2:p+1)=a(1:p); a(1)=1;
--- tp1/levinson2.m
+++ tp1/levinson2.m
@@ -1,4 +1,4 @@
-    function signa_f = levinson2 (ryy)
+
 
     LEN = 20;
 
@@ -7,26 +7,67 @@
     end
 
 
-    %Initialization
-    A = zeros(1,LEN);
-    K = zeros(1,LEN);
-    delta = zeros(1,LEN);
-    sigma_f = zeros(1,LEN);
-  
-    A(1)=1;
+    % initialization
+    %A = zeros(1,LEN);
+    %K = zeros(1,LEN);
+    %delta = zeros(1,LEN);
+    %sigma_f = zeros(1,LEN);
+    AN = zeros(1,LEN);
+    A(1) = 1;
     K(1) = -ryy(2) / ryy(1); 
     delta(1) = ryy(1);
-    
-    
-    for i =1:LEN-1
-        A(i+1) = [A(i) ; 0] + K(1) * [ 0 ; J_func(A(i))*A(i)];
-        delta(i+1)  = ryy(i:-1:1)*A(i);
-        K(n+1) = - (delta(n+1))/(sigma_f);
-        sigma_f(i+1) = (1-power(K(i+1)));
+    sigma_f(1) = ryy(1) + ryy(2)*A(2);%%?
+    S=0;
+    Ek= ryy(1);
+    for k=2:LEN
         
         
+        for j = 1:k
+            S = S - A(j)*ryy(k+1-j); %possibly index minstake
+        end
+        S=S/Ek;
         
+        for n = 1:(k+1)/2
+            tmp = A(n) + S *A(k+1-n);
+            A(k+1-n) = tmp;
+        end
+        Ek = Ek*(1-power(S,2));
     end
+    
+        
+      
+        
+        
+    
+    
+    
+%     S=0;
+%     
+%     for n = 2:LEN-1
+%         for j=1: n-1
+%             S = S + ryy(j)*A(n-j);
+%         end
+%         S = S + ryy(n);
+%         K = -S/sigma_f;
+%         for j=1:n-1
+%             AN(j) = A(j) * K*A(n-j);
+%             disp(AN(j));
+%         end
+%         AN(n) = K;
+%         sigma_f = sigma_f * (1-K^2);
+%     end
+    
+    
+        
+            
+    
+%     for i =1:LEN-1
+%         A(:,i+1) = [A(:,i) ; 0] + K(i+1) * [ 0 ; J_func(A(:,i)'.*A(:,i)')];
+%         delta(i+1)  = ryy(i:-1:1)*A(i);
+%         K(i+1) = - (delta(i+1))/(sigma_f(i));
+%         sigma_f(i+1) = (1-power(K(i+1),2));
+%     end
+
     
     
         
